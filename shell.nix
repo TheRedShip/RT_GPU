e332161e7a793f77e00003dcedd2d62f85c8e641
tomoron <tomoron@student.42angouleme.fr>
2025-02-03 16:53:25 +0100

    add alias to shell.nix to make a dummy x11 session

--- shell.nix
+++ shell.nix
@@ -1,6 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ libGL xorg.libX11 libGLU glfw ffmpeg];
-	shellHook="make -j";
+    nativeBuildInputs = with pkgs; [ libGL xorg.libX11 libGLU glfw ffmpeg xorg.xvfb];
+	shellHook=''
+	alias xdum="Xvfb :99 -screen 0 1920x1080x24 +extension GLX +render -noreset & export DISPLAY=:99";
+	alias xdumstop='pkill Xvfb'
+	make -j;
+	'';
 }
 
