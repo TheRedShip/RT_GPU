92f93ad690e77d4224dfafa4e1473c5819c5c065
tomoron <tomoron@student.42angouleme.fr>
2025-04-21 20:04:00 +0200

    remove git lfs

--- renders/test.sh
+++ renders/test.sh
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cffb5c4319b966658663ee89470c5c8baa45cc503852592fa09c312d3b0f5b30
-size 747
+#!/usr/bin/env bash
+set -e
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 target_size_MB input_file output_file"
+    exit 1
+fi
+
+target_size_mb="$1"
+input_file="$2"
+output_file="$3"
+
+duration=$(ffprobe -v error -select_streams v:0 -show_entries format=duration \
+    -of default=noprint_wrappers=1:nokey=1 "$input_file")
+duration=${duration%.*}  # remove decimals
+target_size_kbit=$((target_size_mb * 8192))  # 1 MB = 8192 kbit
+bitrate=$((target_size_kbit / duration))
+clear
+echo "                                 bitrate : $bitrate"
+ffmpeg -y -i "$input_file" -c:v libx264 -b:v "${bitrate}k" -pass 1 -an -f mp4 /dev/null && \
+ffmpeg -i "$input_file" -c:v libx264 -b:v "${bitrate}k" -pass 2 -c:a aac -b:a 128k "$output_file"
+rm -f ffmpeg2pass-0.log* 
+
